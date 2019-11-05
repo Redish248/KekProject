1091cbbac93c2ef5645c66f2181f976fd3eb47bb
= <=>
2019-11-05 08:55:15 +0300

    i18n

--- kek.js
+++ kek.js
@@ -1,5 +1,28 @@
+var lang = "English";
+
 console.log("kek");
 document.getElementById("name").onchange = (event) => {
     const name = document.getElementById("name").value;
-    document.getElementById("kekElement").innerHTML = name ? `Привет, ${name}! Кек.` : "";
+    if (lang == "Russian")
+        document.getElementById("kekElement").innerHTML = name ? `Привет, ${name}! Кек.` : "";
+    else
+        document.getElementById("kekElement").innerHTML = name ? `Hello, ${name}! Kek.` : "";
+}
+
+function set_eng() {
+    document.getElementById("lang_label").innerHTML = "Language";
+    document.getElementById("name_label").innerHTML = "Input your name:";
+    lang = "English";
+    reset_kek();
+}
+
+function set_rus() {
+    document.getElementById("lang_label").innerHTML = "Язык";
+    document.getElementById("name_label").innerHTML = "Введите ваше имя:";
+    lang = "Russian";
+    reset_kek();
+}
+
+function reset_kek() {
+    document.getElementById("kekElement").innerHTML = "";
 }
